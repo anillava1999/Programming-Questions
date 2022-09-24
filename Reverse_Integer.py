c9388a7c88fc097722f937cf9526207f1f8df8a0
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2022-09-24 21:18:43 +0530

    Create Reverse_Integer.py

---
+++ Reverse_Integer.py
@@ -0,0 +1,9 @@
+## Python program to reverse a number with explanation
+
+Number = 1234
+Reverse = 0
+while(Number > 0) :
+    LastDigit = A % 10   # finding last digit 
+    Reverse = (Reverse * 10) + lastdigit  # adding last digit to variable
+    A = A // 10   # removing last digit in number
+print(reverse)
