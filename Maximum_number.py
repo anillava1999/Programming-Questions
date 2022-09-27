dd0eedea09f7f41d48761ac833e94809bd821d4b
Anil Lavakumar <71332138+anillava1999@users.noreply.github.com>
2022-09-27 09:23:54 +0530

    Create Maximum_number.py

---
+++ Maximum_number.py
@@ -0,0 +1,10 @@
+# Python program to find greatest of three numbers
+
+list_tech = []
+first_no = int(input("Enter first number :"))
+list_tech.append(first_no)
+second_no = int(input("Enter second number :"))
+list_tech.append(second_no)
+third_no = int(input("Enter third number :"))
+list_tech.append(third_no)
+print(max(list_tech))
